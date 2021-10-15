aa47664e083c3436e00d670049f75a08c8e12063
Saryace <saryace@gmail.com>
2021-10-14 23:10:14 -0300

    primer commit

---
+++ libs/typed.js
@@ -0,0 +1,17 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+
+<script>
+const options = {
+  strings: [
+    '<pre>soilArepetition1</pre>', '<pre>soilA-rep2</pre>',
+    '<pre>soilA-replicate3-notreatment</pre>', '<pre>soilA-rep4-treat2</pre>'
+  ],
+  typeSpeed: 60,
+  backSpeed: 20,
+  cursorChar: '',
+  loop: true
+};
+
+const typed = new Typed('.typed', options);
+const typed2 = new Typed('.typed-eg', options);
+</script>
